daf33996599bfbfa17d9b46cf98fc186ed08b8e5
Sonia Guszpit <guszpits@gmail.com>
2022-11-17 11:19:30 +0100

    add clickedElement

--- js/script.js
+++ js/script.js
@@ -6,29 +6,30 @@ document.getElementById('test-button').addEventListener('click', function(){
   */
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
 
     for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [IN PROGRESS] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
+    /* [IN PROGRESS] add class 'active' to the correct article */
   }
   
   const links = document.querySelectorAll('.titles a');
