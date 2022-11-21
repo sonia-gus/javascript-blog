335daa6139b952371050534535fa6b3b24ddbdf2
Sonia Guszpit <guszpits@gmail.com>
2022-11-21 09:23:02 +0100

    start creating new function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById('test-button').addEventListener('click', function(){
     console.log('links:', links);
   });
   */
-
+{
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
@@ -45,3 +45,28 @@ document.getElementById('test-button').addEventListener('click', function(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+  const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+  function generateTitleLinks(){
+
+    /* remove contents of titleList */
+
+    /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+  }
+
+  generateTitleLinks();
+}
